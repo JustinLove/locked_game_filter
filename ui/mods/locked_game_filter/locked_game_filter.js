03eea2e1e908c9d98da42411b991bada5d1abadd
Justin Love <git@JustinLove.name>
2014-12-27 19:15:14 -0600

    filter control

--- ui/mods/locked_game_filter/locked_game_filter.js
+++ ui/mods/locked_game_filter/locked_game_filter.js
@@ -1,9 +1,23 @@
 (function() {
+  model.lockedGameFilter = ko.observable('open');
+
   var baseFilteredGameList = model.filteredGameList
   model.filteredGameList = ko.computed(function() {
-    return baseFilteredGameList().filter(function(game) {
-      return !game.locked
-    })
+    switch (model.lockedGameFilter()) {
+      case 'open': return baseFilteredGameList().filter(function(game) {
+          return !game.locked
+        })
+      case 'locked': return baseFilteredGameList().filter(function(game) {
+          return game.locked
+        })
+      default: return baseFilteredGameList()
+    }
   })
   model.filteredGameList.previous = baseFilteredGameList
+
+  $.get('coui://ui/mods/locked_game_filter/locked_game_filter.html', function(html) {
+    var $html = $(html)
+    ko.applyBindings(model, $html[0])
+    $('.section_controls .form-group:last').after($html[0])
+  })
 })()
